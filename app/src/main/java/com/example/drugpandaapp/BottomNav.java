e554264960f7708967c0fcf4b7f35a54bd8a87fa
Keen Thomas <keenpalarca15@gmail.com>
2024-05-26 09:26:49 +0800

    Main DashBoard Update

--- app/src/main/java/com/example/drugpandaapp/BottomNav.java
+++ app/src/main/java/com/example/drugpandaapp/BottomNav.java
@@ -1,55 +1,24 @@
 package com.example.drugpandaapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class BottomNav extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    private BottomNavigationView bottomNavigationView;
+public class BottomNav extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bottom_nav);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.home:
-                startActivity(new Intent(BottomNav.this, MainDashBoard.class));
-                return true;
-            case R.id.messages:
-                startActivity(new Intent(BottomNav.this, Messages.class));
-                return true;
-            case R.id.notifications:
-                startActivity(new Intent(BottomNav.this, Notifications.class));
-                return true;
-            case R.id.profile:
-                startActivity(new Intent(BottomNav.this, Profile.class));
-                return true;
-            default:
-                return false;
-        }
     }
 }
