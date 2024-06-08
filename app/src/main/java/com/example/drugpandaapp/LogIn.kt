428bec6423b64f452c5004374df4753a4db27ef6
Keen Thomas <keenpalarca15@gmail.com>
2024-06-08 21:43:30 +0800

    Facebook Auth

--- app/src/main/java/com/example/drugpandaapp/LogIn.kt
+++ app/src/main/java/com/example/drugpandaapp/LogIn.kt
@@ -7,12 +7,14 @@ import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -21,6 +23,8 @@ class LogIn : AppCompatActivity() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,11 +37,29 @@ class LogIn : AppCompatActivity() {
             .requestEmail()
             .build()
 
-
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         findViewById<Button>(R.id.btn_googleAccount).setOnClickListener {
             signIn()
+        }
+
+        callbackManager = CallbackManager.Factory.create()
+
+        findViewById<Button>(R.id.btn_fbAccount).setOnClickListener {
+            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email", "public_profile"))
+            LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "Facebook onCancel.")
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "Facebook onError: ${error.message}")
+                }
+            })
         }
     }
 
@@ -49,10 +71,11 @@ class LogIn : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-
                 val account = task.getResult(ApiException::class.java)!!
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
@@ -61,19 +84,34 @@ class LogIn : AppCompatActivity() {
         }
     }
 
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) {task ->
-             if (task.isSuccessful) {
-                 Log.w(TAG, "signInWithCredential:success")
-                 val user = auth.currentUser
-                 updateUI(user)
-             } else {
-                 Log.w(TAG, "signInWithCredential:failure", task.exception)
-                 updateUI(null)
-             }
-        }
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.w(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
     }
 
     private fun updateUI(user: FirebaseUser?) {
@@ -82,9 +120,9 @@ class LogIn : AppCompatActivity() {
             finish()
         }
     }
+
     companion object {
         private const val TAG = "SignInActivity"
         private const val RC_SIGN_IN = 9001
     }
-
 }
