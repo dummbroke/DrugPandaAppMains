7ffce77ef300cc8fc2208ae8c413f7283ea5aca5
Keen Thomas <keenpalarca15@gmail.com>
2024-06-21 18:48:27 +0800

    -Minor UI bugs fixed

--- app/src/main/java/com/example/drugpandaapp/fragments/MessegeFragment.kt
+++ app/src/main/java/com/example/drugpandaapp/fragments/MessegeFragment.kt
@@ -35,7 +35,7 @@ class MessegeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messege, container, false)
+        return inflater.inflate(R.layout.fragment_messages, container, false)
     }
 
     companion object {
